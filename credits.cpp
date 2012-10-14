31be60569fe14b254f797006e62bf07deb441cd3
Simon Pilgrim <extremetuxracer@redking.me.uk>
2012-10-14 17:55:24 +0100

    Converted credits list drawing to use vertex array

--- credits.cpp
+++ credits.cpp
@@ -66,78 +66,125 @@ void LoadCreditList () {
 void DrawCreditsText (double time_step){
     double w = (double)param.x_resolution;
     double h = (double)param.y_resolution;
-	double offs = 0.0;
-	int i;
-	TColor col;
-	if (moving) y_offset += time_step * 30;
+    double offs = 0.0;
+    int i;
+    TColor col;
+    if (moving) y_offset += time_step * 30;
 
-	
-	for (i=0; i < numCredits; i++) {
-		offs = h - 100 - y_offset + CreditList[i].offs;
+    for (i=0; i < numCredits; i++) {
+	offs = h - 100 - y_offset + CreditList[i].offs;
 
-		if (CreditList[i].col == 0) col = colWhite;
-		else col = colDYell;
-		FT.SetColor (col);
-		FT.AutoSizeN (CreditList[i].size);
-		FT.DrawString (-1, (int)offs, CreditList[i].text);
-	}
-
+	col = ( CreditList[i].col == 0 ? colWhite : colDYell );
+	FT.SetColor (col);
+	FT.AutoSizeN (CreditList[i].size);
+	FT.DrawString (-1, (int)offs, CreditList[i].text);
+    }
 
     glDisable (GL_TEXTURE_2D);
-	glColor4dv ((double*)&bgcol);
-    glRectf (0, 0, w, BOTT_Y);
+    glEnableClientState (GL_VERTEX_ARRAY);
+    glEnableClientState (GL_COLOR_ARRAY);
 
-    glBegin( GL_QUADS );
-		glVertex2f (0, BOTT_Y );
-		glVertex2f (w, BOTT_Y );
-		glColor4f (bgcol.r, bgcol.g, bgcol.b, 0 );
-		glVertex2f (w, BOTT_Y + 30 );
-		glVertex2f (0, BOTT_Y + 30 );
-    glEnd();
+    const GLfloat col1[] = {
+        bgcol.r, bgcol.g, bgcol.b, bgcol.a,
+        bgcol.r, bgcol.g, bgcol.b, bgcol.a,
+        bgcol.r, bgcol.g, bgcol.b, bgcol.a,
+        bgcol.r, bgcol.g, bgcol.b, bgcol.a,
+    };
+    const GLfloat col2[] = {
+        bgcol.r, bgcol.g, bgcol.b, bgcol.a,
+        bgcol.r, bgcol.g, bgcol.b, bgcol.a,
+        bgcol.r, bgcol.g, bgcol.b, 0,
+        bgcol.r, bgcol.g, bgcol.b, 0
+    };
 
-    glColor4dv ((double*)&bgcol);
-    glRectf (0, h - TOP_Y, w, h );
-    
-	glBegin( GL_QUADS );
-		glVertex2f (w, h - TOP_Y );
-		glVertex2f (0, h - TOP_Y );
-		glColor4f (bgcol.r, bgcol.g, bgcol.b, 0 );
-		glVertex2f (0, h - TOP_Y - 30 );
-		glVertex2f (w, h - TOP_Y - 30 );
-    glEnd();
-    
-	glColor4f (1, 1, 1, 1 );
+    const GLfloat vtx1[] = {
+        0, 0,
+        w, 0,
+        w, BOTT_Y,
+        0, BOTT_Y
+    };
+    const GLfloat vtx2[] = {
+        0, BOTT_Y,
+        w, BOTT_Y,
+        w, BOTT_Y + 30,
+        0, BOTT_Y + 30
+    };
+
+    glColorPointer(4, GL_FLOAT, 0, col1);
+    glVertexPointer(2, GL_FLOAT, 0, vtx1);
+    glDrawArrays(GL_TRIANGLE_FAN,0,4);
+
+    glColorPointer(4, GL_FLOAT, 0, col2);
+    glVertexPointer(2, GL_FLOAT, 0, vtx2);
+    glDrawArrays(GL_TRIANGLE_FAN,0,4);
+
+    const GLfloat vtx3[] = {
+        0, h - TOP_Y,
+        w, h - TOP_Y,
+        w, h,
+        0, h
+    };
+    const GLfloat vtx4[] = {
+        w, h - TOP_Y,
+        0, h - TOP_Y,
+        0, h - TOP_Y - 30,
+        w, h - TOP_Y - 30
+    };
+
+    glColorPointer(4, GL_FLOAT, 0, col1);
+    glVertexPointer(2, GL_FLOAT, 0, vtx3);
+    glDrawArrays(GL_TRIANGLE_FAN,0,4);
+
+    glColorPointer(4, GL_FLOAT, 0, col2);
+    glVertexPointer(2, GL_FLOAT, 0, vtx4);
+    glDrawArrays(GL_TRIANGLE_FAN,0,4);
+
+    glColor4f (1, 1, 1, 1 );
+
+    glDisableClientState (GL_COLOR_ARRAY);
+    glDisableClientState (GL_VERTEX_ARRAY);
     glEnable (GL_TEXTURE_2D);
-	if (offs < TOP_Y) y_offset = 0;
+    if (offs < TOP_Y) y_offset = 0;
 }
 
 void DrawBackLogo (int x, int y, double size) {
-	GLint w, h;
-	GLfloat width, height, top, bott, left, right;
+    GLint w, h;
+    GLfloat width, height, top, bott, left, right;
 
-	glEnable (GL_TEXTURE_2D);
-	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glBindTexture (GL_TEXTURE_2D, Tex.TexID (T_TITLE));
+    glEnable (GL_TEXTURE_2D);
+    glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glBindTexture (GL_TEXTURE_2D, Tex.TexID (T_TITLE));
 
-	glGetTexLevelParameteriv (GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &w);
-	glGetTexLevelParameteriv (GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &h);
+    glGetTexLevelParameteriv (GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &w);
+    glGetTexLevelParameteriv (GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &h);
 
-	width  = w * size;
-	height = h * size;
+    width  = w * size;
+    height = h * size;
 
-	top = param.y_resolution - y;
-	bott = top - height;
+    top = param.y_resolution - y;
+    bott = top - height;
 
-	if (x >= 0) left = x; else left = (param.x_resolution - width) / 2;
-	right = left + width;
+    left = ( x >= 0 ? x : (param.x_resolution - width) / 2 );
+    right = left + width;
+
+    const GLfloat tex[] = { 0,0, 1,0, 1,1, 0,1 };
+    const GLfloat vtx[] = {
+         left, bott,
+        right, bott,
+        right, top,
+         left, top
+    };
+
+    glEnableClientState (GL_VERTEX_ARRAY);
+    glEnableClientState (GL_TEXTURE_COORD_ARRAY);
 
     glColor4f (1.0, 1.0, 1.0, 0.4);
-	glBegin (GL_QUADS);
-	    glTexCoord2f (0, 0); glVertex2f (left, bott);
-	    glTexCoord2f (1, 0); glVertex2f (right, bott);
-	    glTexCoord2f (1, 1); glVertex2f (right, top);
-	    glTexCoord2f (0, 1); glVertex2f (left, top);
-	glEnd();
+    glVertexPointer(2, GL_FLOAT, 0, vtx);
+    glTexCoordPointer(2, GL_FLOAT, 0, tex);
+    glDrawArrays(GL_TRIANGLE_FAN,0,4);
+
+    glDisableClientState (GL_TEXTURE_COORD_ARRAY);
+    glDisableClientState (GL_VERTEX_ARRAY);
 }
 
 void CreditsKeys (unsigned int key, bool special, bool release, int x, int y){
@@ -156,13 +203,13 @@ static void CreditsMouseFunc (int button, int state, int x, int y ){
 void CreditsMotionFunc (int x, int y ){
     TVector2 old_pos;
 	
-	if (Winsys.ModePending ()) return; 	    
+    if (Winsys.ModePending ()) return; 	    
     y = param.y_resolution - y;
     old_pos = cursor_pos;
     cursor_pos = MakeVector2 (x, y);
 
     if (old_pos.x != x || old_pos.y != y) {
-		if (param.ui_snow) push_ui_snow (cursor_pos);
+	if (param.ui_snow) push_ui_snow (cursor_pos);
     }
 }
 
